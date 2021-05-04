554215362ebc212a05f757a3f29342e3f17f97d0
suyash-wagh <suyashwagh22@gmail.com>
2021-05-04 23:15:17 +0530

    cors done

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path= require('path');
-
+const cors= require('cors');
 const PORT=process.env.PORT || 3000;
 
 const connectDB = require('./config/db');
 connectDB();
+const corsOptions ={
+    origin= process.env.ALLOWED_CLIENTS.split(',');
+}
+app.use(cors(corsOptions));
 app.use(express.static('public'));
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
