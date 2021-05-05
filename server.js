d9ecf4ccbd52fc9b73afc58fbdeb2b1e93cac32f
Suyash Wagh <suyashwagh22@gmail.com>
2021-05-05 23:25:33 +0530

    change1

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ app.set('view engine', 'ejs');
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
 })
