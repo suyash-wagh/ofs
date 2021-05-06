d09361baca423376c27d768f4e13b5e4653aa93b
Suyash Wagh <suyashwagh22@gmail.com>
2021-05-06 15:14:16 +0530

    project

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT=process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
 const corsOptions ={
-    origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300', 'http://localhost:3001', 'https://online-file-sharing.000webhostapp.com/']
+    origin: 'http://localhost:3001'
     //origin: process.env.ALLOWED_CLIENTS.split(',')
 }
 app.use(cors(corsOptions));
@@ -18,11 +18,7 @@ app.set('view engine', 'ejs');
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://online-file-sharing.000webhostapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
 })
